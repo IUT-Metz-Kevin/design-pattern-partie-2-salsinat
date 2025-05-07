9bda09f7906b3df1ce1473946e397ce554db9a48
stanislas <stanislas.guilleray9@etu.univ-lorraine.fr>
2025-05-07 11:56:54 +0200

    ajout de scenario A

--- src/scenario_A.ts
+++ src/scenario_A.ts
@@ -1,1 +1,22 @@
 // Etat
+
+interface PlayerBehavior {
+    move(): void;
+    jump(): void;
+    attack(): void;
+}
+
+class Player implements PlayerBehavior {
+    private _name: string;
+    private _state: State;
+
+    public constructor(name: string) {
+        this.name = name;
+        this.state = new IdlingState(this);
+    }
+
+    public get name(): string { return this._name; }
+    public set name(name: string) {
+        if (!/[a-zA-Z]{2,15}/.test(name))
+    }
+}
